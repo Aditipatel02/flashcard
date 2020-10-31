ee842bdd61e146c3d1debbf51904ee11baf66182
Aditipatel02 <56363123+Aditipatel02@users.noreply.github.com>
2020-10-31 22:23:07 +0530

    get api

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,33 +1,35 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Flashcardlist from './components/Flashcardlist';
 import '../src/styles.css';
+import axios from 'axios';
+
 
 function App() {
-  const [flashcards,setFlashcards]=useState(SAMPLE_FLASHCARDS)
-  return (
-   <Flashcardlist flashcards={flashcards}></Flashcardlist>
-  );
-}
+  const [flashcards, setFlashcards] = useState([])
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = questionItem.correct_answer
+            const options = [...questionItem.incorrect_answers, answer]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: questionItem.question,
+              answer: answer,
+              options: options.sort(() => Math.random() - .5)
+            }
+          }))
+        })
+      }, [])
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id:1,
-    question: '2+2 is?',
-    ans:'4',
-    options:[
-      '2','3','4','5'
-    ]
-  },
-  {
-    id:2,
-    question: '2+3 is?',
-    ans:'5',
-    options:[
-      '2','3','4','5'
-    ]
+    return (
+      <Flashcardlist flashcards={flashcards}></Flashcardlist>
+    );
   }
 
-]
+
 
 export default App;
